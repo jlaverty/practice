1cd91e8293bbcdbd9de1eb5eb9cf838f43f3b76d
James Laverty <jamespjlaverty@gmail.com>
2018-02-10 20:47:07 -0500

    Added links and basic validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,22 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
-  end
-
-  private
-  def article_params
-    params.require(:article.permit(:title, :text))
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 end
+
+private
+def article_params
+  params.require(:article).permit(:title, :text)
+end
+
