1903762b2b1abf8a97c0c11d8907b02350669203
James Laverty <jamespjlaverty@gmail.com>
2018-02-11 12:03:48 -0500

    Added ability to delete and fixed issue with new/edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -30,6 +34,14 @@ class ArticlesController < ApplicationController
       render; edit;
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
 end
 
 private
